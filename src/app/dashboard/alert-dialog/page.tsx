b00179bdee61de485400c9234029482270b49e48
Marco Mora <proveedorcrm@ufinet.com>
2024-05-29 20:24:49 -0600

    Fixing the imports

--- src/app/dashboard/alert-dialog/page.tsx
+++ src/app/dashboard/alert-dialog/page.tsx
@@ -1,38 +1,48 @@
-"use client"
-import { AlertDialogHeader, AlertDialogFooter } from "@/components/ui/alert-dialog"
+'use client'
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 import { Button } from "@/components/ui/button"
-import { AlertDialog, AlertDialogTrigger, AlertDialogContent, AlertDialogTitle, AlertDialogDescription, AlertDialogCancel, AlertDialogAction } from "@radix-ui/react-alert-dialog"
 import { useState } from "react"
 
-
-
-
-
 export default function AlertDialogDemo() {
-    const [dialogOpen, setDialogOpen] = useState(false)
+
+    const [dialogOpen, setDialogOpen] = useState<boolean>(false)
+
     return (
-        <AlertDialog
-            open={dialogOpen}
-        >
-            <AlertDialogTrigger asChild>
-                <Button
-                    onClick={() => setDialogOpen(!dialogOpen)}
-                    variant="outline"
-                >Show Dialog</Button>
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-                <AlertDialogHeader>
-                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                    <AlertDialogDescription>
-                        This action cannot be undone. This will permanently delete your
-                        account and remove your data from our servers.
-                    </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                    <AlertDialogCancel onClick={() => console.log('Cancelando')}>Cancel</AlertDialogCancel>
-                    <AlertDialogAction onClick={() => console.log('Continuando')}>Continue</AlertDialogAction>
-                </AlertDialogFooter>
-            </AlertDialogContent>
-        </AlertDialog>
+        <div className="grid grid-cols-2 gap-4">
+            <AlertDialog
+                open={dialogOpen}
+                onOpenChange={setDialogOpen}
+            >
+                <AlertDialogTrigger asChild>
+                    <Button variant="outline">Show Dialog</Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                        <AlertDialogDescription>
+                            This action cannot be undone. This will permanently delete your
+                            account and remove your data from our servers.
+                        </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction>Continue</AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
+            <Button
+                onClick={() => setDialogOpen(!dialogOpen)}
+            >Open Dialog</Button>
+        </div>
     )
 }
